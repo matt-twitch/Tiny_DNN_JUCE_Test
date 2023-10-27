e0dcd329c25a475677ad8fbe059f078370dfe00a
Matt Twitchen <matthewtwitchen@gforcesoftware.com>
2023-10-27 11:06:04 +0100

    added files

---
+++ Tiny_DNN_Test_2/Source/Main.cpp
@@ -0,0 +1,20 @@
+/*
+  ==============================================================================
+
+    This file contains the basic startup code for a JUCE application.
+
+  ==============================================================================
+*/
+
+#include <JuceHeader.h>
+#include <tiny_dnn.h>
+
+//==============================================================================
+int main (int argc, char* argv[])
+{
+
+    // ..your code goes here!
+
+
+    return 0;
+}

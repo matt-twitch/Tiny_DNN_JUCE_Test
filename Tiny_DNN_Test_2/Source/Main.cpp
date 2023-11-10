5c3989b4d5bf3f7a53be766d0382fbbfdbf8c503
Matt Twitchen <matthewtwitchen@gforcesoftware.com>
2023-11-10 14:16:28 +0000

    addition: adsr generators

--- Tiny_DNN_Test_2/Source/Main.cpp
+++ Tiny_DNN_Test_2/Source/Main.cpp
@@ -14,21 +14,74 @@
 
 //std::vector<tiny_dnn::label_t> parse_Labels
 
-void construct_rnn()
+std::vector<float> generatePad()
 {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    
+    std::uniform_int_distribution<> att(0.1, 0.3);
+    std::uniform_int_distribution<> dec(0.2, 0.5);
+    std::uniform_int_distribution<> sus(0.1, 0.3);
+    std::uniform_int_distribution<> rel(0.1, 0.3);
+    
+    float attack = att(gen);
+    float decay = dec(gen);
+    float sustain = sus(gen);
+    float release = rel(gen);
+    
+    std::vector<float> pad {attack, decay, sustain, release};
+    
+    return pad;
+}
 
+std::vector<float> generateLead()
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    
+    std::uniform_int_distribution<> att(0.3, 0.9);
+    std::uniform_int_distribution<> dec(0.3, 0.8);
+    std::uniform_int_distribution<> sus(0.4, 0.7);
+    std::uniform_int_distribution<> rel(0.6, 0.9);
+    
+    float attack = att(gen);
+    float decay = dec(gen);
+    float sustain = sus(gen);
+    float release = rel(gen);
+    
+    std::vector<float> lead {attack, decay, sustain, release};
+    
+    return lead;
+}
+
+void construct_rnn()
+{    
     const int num_features = 4; // Number of input features, equivalent to sequence length
     const int num_vals = 4; // Number of possible values, equivalent to vocab size
     const int hidden_size = 128; // size of hidden layers
     
+    /*
     std::vector<tiny_dnn::vec_t> values    {{0, 1, 2, 3}, {0, 1, 2, 3}, {0, 1, 2, 3}, {0, 1, 2, 3}, {0, 1, 2, 3},
                                             {0, 1, 2, 3}, {0, 1, 2, 3}, {0, 1, 2, 3}, {0, 1, 2, 3}, {0, 1, 2, 3},
                                             {0, 1, 2, 3}, {0, 1, 2, 3}, {0, 1, 2, 3}, {0, 1, 2, 3}, {0, 1, 2, 3},
                                             {0, 1, 2, 3}, {0, 1, 2, 3}, {0, 1, 2, 3}, {0, 1, 2, 3}, {0, 1, 2, 3}};
-    std::vector<tiny_dnn::label_t> labels  {2, 4, 6, 3, 1, 3, 7, 4, 3, 2, 5, 2, 4, 5, 4, 1, 1, 3, 8, 5,
-                                            2, 4, 6, 3, 3, 2, 5, 2, 4, 5, 4, 7, 4, 5, 1, 8, 2, 4, 6, 7,
-                                            2, 4, 6, 3, 1, 3, 7, 4, 3, 2, 5, 2, 4, 5, 4, 1, 1, 3, 8, 5,
-                                            2, 4, 6, 3, 3, 2, 5, 2, 4, 5, 4, 7, 4, 5, 1, 8, 2, 4, 6, 7};
+    std::vector<tiny_dnn::label_t> labels  {{2, 4, 6, 3}, {1, 3, 7, 4}, {3, 2, 5, 2}, {4, 5, 4, 1}, {1, 3, 8, 5},
+                                            {2, 4, 6, 3}, {3, 2, 5, 2}, {4, 5, 4, 7}, {4, 5, 1, 8}, {2, 4, 6, 7},
+                                            {2, 4, 6, 3}, {1, 3, 7, 4}, {3, 2, 5, 2}, {4, 5, 4, 1}, {1, 3, 8, 5},
+                                            {2, 4, 6, 3}, {3, 2, 5, 2}, {4, 5, 4, 7}, {4, 5, 1, 8}, {2, 4, 6, 7}};
+    */
+    
+    std::vector<tiny_dnn::vec_t> values {{0.2, 0.4, 0.6, 0.3}, {0.1, 0.3, 0.7, 0.4}, {0.3, 0.2, 0.5, 0.2}, {0.4, 0.5, 0.4, 0.1}, {0.1, 0.3, 0.8, 0.5},
+                                         {0.2, 0.4, 0.6, 0.3}, {0.3, 0.2, 0.5, 0.2}, {0.4, 0.5, 0.4, 0.7}, {0.4, 0.5, 0.1, 0.8}, {0.2, 0.4, 0.6, 0.7},
+                                         {0.2, 0.4, 0.6, 0.3}, {0.1, 0.3, 0.7, 0.4}, {0.3, 0.2, 0.5, 0.2}, {0.4, 0.5, 0.4, 0.1}, {0.1, 0.3, 0.8, 0.5},
+                                         {0.2, 0.4, 0.6, 0.3}, {0.1, 0.3, 0.7, 0.4}, {0.3, 0.2, 0.5, 0.2}, {0.4, 0.5, 0.4, 0.1}, {0.1, 0.3, 0.8, 0.5},
+                                         {0.2, 0.4, 0.6, 0.3}, {0.3, 0.2, 0.5, 0.2}, {0.4, 0.5, 0.4, 0.7}, {0.4, 0.5, 0.1, 0.8}, {0.2, 0.4, 0.6, 0.7}};
+    
+    std::vector<tiny_dnn::label_t> labels {0, 1, 0, 1, 0,
+                                           0, 1, 0, 1, 0,
+                                           0, 1, 0, 1, 0,
+                                           0, 1, 0, 1, 0,
+                                           0, 1, 0, 1, 0};
     
     tiny_dnn::network<tiny_dnn::sequential> nn;
     tiny_dnn::core::backend_t backend_type = tiny_dnn::core::default_engine();
@@ -49,6 +102,12 @@ void construct_rnn()
     int epochs = 30;
     
     nn.train<tiny_dnn::mse>(opt, values, labels, batch_size, epochs);
+    
+    tiny_dnn::vec_t input {2, 4, 6, 3};
+    tiny_dnn::vec_t result = nn.predict(input);
+    
+    for(int i = 0 ; i< result.size() ; i++)
+        DBG("result " << i << " = " << result[i]);
     
 }
 
